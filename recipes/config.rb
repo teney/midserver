93076d435f6a48cafebf8cc6f6f7b7b343f47d59
Frank Teney <frank.teney@gmail.com>
2015-12-12 13:54:58 -0500

    Add template for config.xml

---
+++ recipes/config.rb
@@ -0,0 +1,8 @@
+config = node['default']['install_base'] + '/agent/config.xml'
+
+template config do
+  source 'config-xml.erb'
+  :create_if_missing
+  notifies :restart, 'service[snc-midserver]', immediately
+end
+  
