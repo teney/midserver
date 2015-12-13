fd3bb8fa2e7c1354bbd84bdd2ac2ff45fb378f3d
Frank Teney <frank.teney@gmail.com>
2015-12-13 08:05:39 -0500

    Added variables in config-erb.xml.
    Added attribute instance_name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['midserver']['url'] = "https://instance.servicenow.com"
 default['midserver']['user'] = "midserver"
 default['midserver']['password'] = 'password'
+default['midserver']['instance_name'] = 'My MID Server'
 default['midserver']['install_base'] = '/tmp/midserver'
 default['midserver']['download_url'] = 'http://install.service-now.com/glide/distribution/builds/package/mid/2015/06/08/mid.2015-06-08-1310.linux.x86-32.zip'
 default['midserver']['zipfile'] = '/tmp/sncmid.zip'
