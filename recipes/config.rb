18c85e6c70b7396a489b6136f11491fec1e063a5
Frank Teney <frank.teney@gmail.com>
2015-12-13 21:10:02 -0500

    Added template info

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,7 +1,7 @@
-#config = node['default']['install_base'] + '/agent/config.xml'
+config = node['midserver']['install_base'] + '/agent/config.xml'
 
-#template config do
-#  source 'config-xml.erb'
-#  :create_if_missing
-#  notifies :restart, 'service[snc-midserver]', immediately
-#end
+template config do
+  source 'config-xml.erb'
+  :create_if_missing
+  notifies :restart, "service[#{node['midserver']['service_name']}], immediately
+end
